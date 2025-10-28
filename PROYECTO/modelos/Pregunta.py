dbf14191e16ae527a2a2fcb50648fa410adcab8b
MatiasEmmanuelGonzalez <emmatygonza@gmail.com>
2025-10-28 10:25:34 -0300

    Add files via upload

---
+++ PROYECTO/modelos/Pregunta.py
@@ -0,0 +1,11 @@
+# modelos/Pregunta.py
+
+class Pregunta:
+    def __init__(self, id, pregunta, respuesta_correcta, op1, op2, op3, categoria, dificultad, juego_id):
+        self.id = id
+        self.pregunta = pregunta
+        self.respuesta_correcta = respuesta_correcta
+        self.opciones_incorrectas = [op1, op2, op3] 
+        self.categoria = categoria
+        self.dificultad = dificultad
+        self.juego_id = juego_id
